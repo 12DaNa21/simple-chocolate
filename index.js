7b3076d339efbb78096a80a057b1a8132876f251
12DaNa21 <145206474+12DaNa21@users.noreply.github.com>
2024-03-23 19:50:59 +0200

    created SC

---
+++ index.js
@@ -0,0 +1,39 @@
+const showFormBtn = document.getElementById('showFormBtn');
+const overlay = document.getElementById('overlay');
+const formContainer = document.getElementById('formContainer');
+const closeButton = document.getElementById('closeButton');
+
+function toggleMenu() {
+  var menu = document.getElementById("myDiv");
+  menu.classList.toggle("show-menu");
+}
+document.querySelector('.header-menu-btn').addEventListener('click', function() {
+  toggleMenu(); 
+});
+
+function closeMenu() {
+  var menu = document.getElementById("myDiv");
+  menu.classList.remove("show-menu");
+}
+
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+  closeMenu(); 
+});
+
+showFormBtn.addEventListener('click', function() {
+  overlay.style.display = 'block';
+  formContainer.style.display = 'block';
+});
+
+overlay.addEventListener('click', function(event) {
+  if (event.target === overlay) {
+    overlay.style.display = 'none';
+    formContainer.style.display = 'none';
+  }
+});
+
+closeButton.addEventListener('click', function() {
+  overlay.style.display = 'none';
+  formContainer.style.display = 'none';
+});
